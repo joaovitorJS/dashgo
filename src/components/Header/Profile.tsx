979823a790f7d08bdad3bc90ee22fa96c3fa8fef
joaovitorJS <joao.vitordeoliveira@hotmail.com>
2021-12-23 10:26:30 -0400

    Header, Sidebar, Dashboard responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -5,18 +5,24 @@ import {
   Text
 } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex
       align="center"
       ml="auto"
     >
-      <Box mr="4" textAlign="right">
-        <Text>João Vitor</Text>
-        <Text color="gray.300" fontSize="small">
-          joao.vitordeoliveira@hotmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>João Vitor</Text>
+          <Text color="gray.300" fontSize="small">
+            joao.vitordeoliveira@hotmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="João Vitor" src="https://github.com/joaovitorJS.png" />
     </Flex>
