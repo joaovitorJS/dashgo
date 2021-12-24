2715b8558a549ffde73444fb840118ef66094095
joaovitorJS <joao.vitordeoliveira@hotmail.com>
2021-12-23 22:23:40 -0400

    Navigation & ActiveLink

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -8,6 +8,7 @@ import {
   SimpleGrid,
   Button
 } from '@chakra-ui/react';
+import Link from 'next/link';
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
@@ -53,10 +54,11 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">
-                Cancelar
-              </Button>
-
+              <Link href="/users">
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme="pink">
                 Salvar
               </Button>
