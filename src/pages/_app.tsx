b95d2de430150983185afd54a173a16ee3e36dbb
joaovitorJS <joao.vitordeoliveira@hotmail.com>
2021-12-27 09:50:51 -0400

    Config: React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,24 @@ import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../contexts/SidebaDrawerContext';
 import { makeeServer } from '../services/mirage';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 if (process.env.NODE_ENV === 'development') {
   makeeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>  
+    <QueryClientProvider client={queryClient}>
+
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>  
+    </QueryClientProvider>
   );
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import Link from 'next/link';
 import { 
   Box, 
   Text,
@@ -13,9 +14,11 @@ import {
   Checkbox,
   Tbody,
   Td, 
-  useBreakpointValue
+  useBreakpointValue, 
+  Spinner
 } from '@chakra-ui/react';
-import Link from 'next/link';
+
+import { useQuery } from 'react-query';
 
 import { RiAddLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
@@ -24,16 +27,19 @@ import { Sidebar } from '../../components/Sidebar';
 
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+    
+    return data;
+  });
+
+
+  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-    .then(response => response.json())
-    .then(data => console.log(data));
-  }, []);
   
   return (  
     <Box>
@@ -70,79 +76,92 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>
-                  Usuário
-                </Th>
-                { isWideVersion &&
-                  <Th>
-                    Data de cadastro
-                  </Th>
-                }
+          { isLoading ? (
+              <Flex justify="center">
+                <Spinner />
+              </Flex>
+            ) : error ? (
+              <Flex  justify="center">
+                <Text>Falha ao obter dados dos usuários.</Text>
+              </Flex>
+            ) : (
+              <>
+                <Table colorScheme="whiteAlpha">
+                  <Thead>
+                    <Tr>
+                      <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                        <Checkbox colorScheme="pink" />
+                      </Th>
+                      <Th>
+                        Usuário
+                      </Th>
+                      { isWideVersion &&
+                        <Th>
+                          Data de cadastro
+                        </Th>
+                      }
 
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">João Vitor</Text>
-                    <Text fontSize="sm" color="gray.300">joao.vitordeoliveira@hotmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && 
-                  <Td>
-                    04 de Abril, 2021
-                  </Td>
-                }
-              </Tr>
+                      <Th width="8"></Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">João Vitor</Text>
+                          <Text fontSize="sm" color="gray.300">joao.vitordeoliveira@hotmail.com</Text>
+                        </Box>
+                      </Td>
+                      { isWideVersion && 
+                        <Td>
+                          04 de Abril, 2021
+                        </Td>
+                      }
+                    </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">João Vitor</Text>
-                    <Text fontSize="sm" color="gray.300">joao.vitordeoliveira@hotmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && 
-                  <Td>
-                    04 de Abril, 2021
-                  </Td>
-                }
-              </Tr>
+                    <Tr>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">João Vitor</Text>
+                          <Text fontSize="sm" color="gray.300">joao.vitordeoliveira@hotmail.com</Text>
+                        </Box>
+                      </Td>
+                      { isWideVersion && 
+                        <Td>
+                          04 de Abril, 2021
+                        </Td>
+                      }
+                    </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">João Vitor</Text>
-                    <Text fontSize="sm" color="gray.300">joao.vitordeoliveira@hotmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && 
-                  <Td>
-                    04 de Abril, 2021
-                  </Td>
-                }
-              </Tr>
-            </Tbody>
-          </Table>
+                    <Tr>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">João Vitor</Text>
+                          <Text fontSize="sm" color="gray.300">joao.vitordeoliveira@hotmail.com</Text>
+                        </Box>
+                      </Td>
+                      { isWideVersion && 
+                        <Td>
+                          04 de Abril, 2021
+                        </Td>
+                      }
+                    </Tr>
+                  </Tbody>
+                </Table>
 
-          <Pagination />
+                <Pagination />
+              </>
+            )
+          }
         </Box>
       </Flex>
     </Box>
