7725165e0a98a2f7d5988b558291a8016a8445a2
joaovitorJS <joao.vitordeoliveira@hotmail.com>
2021-12-28 08:53:12 -0400

    Component paging and Switching pages

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,39 @@
-import { Stack, Button, Box } from '@chakra-ui/react';
+import { Stack, Box, Text } from '@chakra-ui/react';
 import { PaginationItem } from './PaginationItem';
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegister: number;
+  registerPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to-from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter(page => page > 0)
+}
+
+export function Pagination({
+  totalCountOfRegister, 
+  currentPage = 1, 
+  registerPerPage = 10,
+  onPageChange
+}: PaginationProps) {
+
+  const lastPage = Math.floor(totalCountOfRegister / registerPerPage);
+  const previousPages = currentPage > 1
+    ? generatePagesArray(currentPage-1-siblingsCount, currentPage - 1)
+    : [];
+
+  const nextPages = currentPage < lastPage
+    ? generatePagesArray(currentPage, Math.min(currentPage + siblingsCount, lastPage))
+    : [];
+
   return (
     <Stack
       direction={["column", "row"]}
@@ -11,16 +43,41 @@ export function Pagination() {
       spacing="6"
     >
       <Box>
-        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+        <strong>{(currentPage-1)*registerPerPage}</strong> - <strong>{(currentPage-1)*registerPerPage + registerPerPage}</strong> de <strong>{totalCountOfRegister}</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
-        <PaginationItem number={5} />
-        <PaginationItem number={6} />
-        <PaginationItem number={7} />
+
+        {currentPage > (1+siblingsCount) && (
+          <>
+            <PaginationItem number={1} onPageChange={onPageChange}/>
+            { currentPage > (2 + siblingsCount) && (
+              <Text color="gray.300" w="8" textAlign="center">...</Text>
+            )}
+          </>
+        )}
+
+        {previousPages.length > 0 && previousPages.map(page => {
+          return (
+            <PaginationItem key={page} number={page} onPageChange={onPageChange}/>
+          );
+        })}
+
+        <PaginationItem number={currentPage} onPageChange={onPageChange} isCurrent/>
+
+        {nextPages.length > 0 && nextPages.map(page => {
+          return (
+            <PaginationItem key={page} number={page} onPageChange={onPageChange}/>
+          );
+        })}
+
+        {(currentPage + siblingsCount) < lastPage && (
+          <>
+            { (currentPage + 1 + siblingsCount) < lastPage && (
+              <Text color="gray.300" w="8" textAlign="center">...</Text>
+            )}
+            <PaginationItem number={lastPage} onPageChange={onPageChange}/>
+          </>
+        )}
       </Stack>
     </Stack>
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,10 +22,12 @@ import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 import { useUsers } from '../../services/hooks/useUsers';
+import { useState } from 'react';
 
 
 export default function UserList() {
-  const { data, isLoading, error, isFetching } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, error, isFetching } = useUsers(page);
 
 
   const isWideVersion = useBreakpointValue({
@@ -100,7 +102,7 @@ export default function UserList() {
                     </Tr>
                   </Thead>
                   <Tbody>
-                    {data.map(user => {
+                    {data.users.map(user => {
                       return (
                         <Tr key={user.id}>
                           <Td px={["4", "4", "6"]}>
@@ -124,7 +126,11 @@ export default function UserList() {
                   </Tbody>
                 </Table>
 
-                <Pagination />
+                <Pagination 
+                  totalCountOfRegister={data.totalCount}
+                  currentPage={page}
+                  onPageChange={setPage}
+                />
               </>
             )
           }
